4644d67c3d4788580b2058b262c0b628f86845be
acontass <acontass@student.42.fr>
2018-01-25 12:49:44 +0100

    Initial commit Catmash

---
+++ Catmash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catmash
+//
+//  Created by acontass on 25/01/2018.
+//  Copyright © 2018 acontass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
